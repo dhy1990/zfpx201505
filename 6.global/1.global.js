ffd9c26115622a76d2d32e43548d79f572c09021
dhy1990 <469089157@qq.com>
2015-08-02 10:45:34 +0800

    global and console

---
+++ 6.global/1.global.js
@@ -0,0 +1,5 @@
+//console.log(global);
+//当前脚本
+console.log(__filename);
+//当前脚本所在的目录
+console.log(__dirname);
---
+++ 6.global/2.console.js
@@ -0,0 +1,10 @@
+/**
+ * 控制台
+ * */
+//log 标准输出
+console.log('this is a log');
+//重定向到文件里
+console.log('%s',"zfpx"); //string
+console.log('%s',{name:'zfpx'});//string
+console.log('%j',{name:'zfpx'});//json
+console.log('%d','zfpx');//参数转化为数字
